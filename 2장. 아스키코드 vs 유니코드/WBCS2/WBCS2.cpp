7c8b6887e9c989db1db71bc30c02142127dbadf2
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-24 19:35:00 +0900

    STUDY: WBCS2

---
+++ 2장. 아스키코드 vs 유니코드/WBCS2/WBCS2.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	wchar_t str[] = L"ABC";
+	int size = sizeof(str);
+	int len = wcslen(str);
+
+	wprintf(L"Array Size : %d \n", size);
+	wprintf(L"String Length : %d \n", len);
+
+	return 0;
+}
