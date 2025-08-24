18e7edb3993a05aa5961ce8729fea0af020eaf1e
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-24 19:35:08 +0900

    STUDY: WBCS4

---
+++ 2장. 아스키코드 vs 유니코드/WBCS4/WBCS4.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int wmain(int argc, wchar_t* argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		fputws(argv[i], stdout);
+		fputws(L"\n", stdout);
+	}
+
+	return 0;
+}
