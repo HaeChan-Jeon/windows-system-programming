e400d5a2af5766b62247ae1e51e7d8f7b1dffb45
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-08-27 07:40:09 +0900

    STUDY: PolymorphicType2

---
+++ 3장. 64비트 기반 프로그래밍/PolymorphicType2/PolymorphicType2.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <tchar.h>
+#include <Windows.h>
+
+UINT_PTR CalDistance(UINT_PTR a, UINT_PTR b)
+{
+    return a - b;
+}
+
+int main()
+{
+    INT32 val1 = 10;
+    INT32 val2 = 20;
+
+    _tprintf(
+        _T("distance : %u \n"),
+        CalDistance((UINT_PTR)&val1, (UINT_PTR)&val2)
+    );
+
+    return 0;
+}
