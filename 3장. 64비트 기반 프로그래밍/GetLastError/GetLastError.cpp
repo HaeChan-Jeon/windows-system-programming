d10a7e64e7386e982214482468d22b412803c9c2
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-08-27 07:39:56 +0900

    STUDY: GetLastError

---
+++ 3장. 64비트 기반 프로그래밍/GetLastError/GetLastError.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+
+int main(void)
+{
+	HANDLE hFile =
+		CreateFile(			// Windows system 함수
+			_T("ABC.DAT"), GENERIC_READ, FILE_SHARE_READ,
+			NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL,
+			NULL);
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		_tprintf(_T("error code: %d \n"), GetLastError());
+		return 0;
+	}
+	return 0;
+}
